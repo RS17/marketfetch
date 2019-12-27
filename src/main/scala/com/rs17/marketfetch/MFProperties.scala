827ac97ec4e4c9f2c292d1ad9f0053383b7f172f
Ravi Suri <rsuriprog@gmail.com>
2019-12-26 21:09:08 -0500

    Update MFProperties.scala

--- src/main/scala/com/rs17/marketfetch/MFProperties.scala
+++ src/main/scala/com/rs17/marketfetch/MFProperties.scala
@@ -2,7 +2,7 @@ package com.rs17.marketfetch
 
 // Configuration object.  I prefer to hard code when possible for easy debugging. Fight me.
 object MFProperties {
-  val repeaterOut: String = "home/ravi/out.txt"
+  val repeaterOut: String = "home/out.txt"
   val repeaterSleepMilli: Int = 501
   val repeaterRefreshMin: Int = 1
 }
